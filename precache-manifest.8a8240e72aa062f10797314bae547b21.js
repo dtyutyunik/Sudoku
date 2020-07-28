ac8249170a76f31bc43d8e406c3fc3e7fd9f5496
Dmitriy <dmitriy.tyutyunik@gmail.com>
2020-07-27 21:13:07 -0400

    running it

---
+++ precache-manifest.8a8240e72aa062f10797314bae547b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df22c2276050464f29057f0b749c6fb",
+    "url": "/Sudoku/index.html"
+  },
+  {
+    "revision": "29786dd9d3756ce4acb3",
+    "url": "/Sudoku/static/css/main.9bb42def.chunk.css"
+  },
+  {
+    "revision": "f485e443459b37289edf",
+    "url": "/Sudoku/static/js/2.bf378a1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sudoku/static/js/2.bf378a1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29786dd9d3756ce4acb3",
+    "url": "/Sudoku/static/js/main.4387436b.chunk.js"
+  },
+  {
+    "revision": "407637988b087a730bce",
+    "url": "/Sudoku/static/js/runtime-main.7ad968fd.js"
+  }
+]);
